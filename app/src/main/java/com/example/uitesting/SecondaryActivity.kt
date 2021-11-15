1b7ce8faabaed25c7573de5f019edc26dd9564aa
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-11-15 20:42:15 +0530

    Learned to check if Things are displayed and the test is correct or not

--- app/src/main/java/com/example/uitesting/SecondaryActivity.kt
+++ app/src/main/java/com/example/uitesting/SecondaryActivity.kt
@@ -4,16 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_secondary.*
 
-class MainActivity : AppCompatActivity() {
+class SecondaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_secondary)
 
-        button_next_activity.setOnClickListener {
-            val intent = Intent(this, SecondaryActivity::class.java)
-
-            startActivity(intent)
+        button_back.setOnClickListener {
+            onBackPressed()
         }
     }
 }
