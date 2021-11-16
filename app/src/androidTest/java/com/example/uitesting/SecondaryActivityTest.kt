4396db8bbacc94488996d07368143d5e70cc62dd
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-11-16 19:59:39 +0530

    Learned to check for navigation between activities

--- app/src/androidTest/java/com/example/uitesting/SecondaryActivityTest.kt
+++ app/src/androidTest/java/com/example/uitesting/SecondaryActivityTest.kt
@@ -3,11 +3,13 @@ package com.example.uitesting
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Assert.*
@@ -18,27 +20,18 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class SecondaryActivityTest
 {
-    @get:Rule
-    val activityRule = ActivityScenarioRule(SecondaryActivity::class.java)
-
     @Test
-    fun test_isActivityInView() {
-        onView(ViewMatchers.withId(R.id.secondary))
-            .check(matches(isDisplayed()))
-    }
+    fun test_back_press_to_mainActivity() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
-    @Test
-    fun test_visiblity_title_backButton() {
-        onView(ViewMatchers.withId(R.id.activity_secondary_title))
-            .check(matches(isDisplayed()))
+        onView(ViewMatchers.withId(R.id.button_next_activity)).perform(ViewActions.click())
 
-        onView(ViewMatchers.withId(R.id.button_back))
-            .check(matches(isDisplayed()))
-    }
+        onView(ViewMatchers.withId(R.id.secondary)).check(matches(isDisplayed()))
 
-    @Test
-    fun test_isTitleTestCorrect() {
-        onView(ViewMatchers.withId(R.id.activity_secondary_title))
-            .check(matches(withText(R.string.text_secondaryactivity)))
+        //onView(withId(R.id.button_back)).perform(click()) // method 1
+
+        pressBack()
+
+        onView(ViewMatchers.withId(R.id.main)).check(matches(isDisplayed()))
     }
 }
