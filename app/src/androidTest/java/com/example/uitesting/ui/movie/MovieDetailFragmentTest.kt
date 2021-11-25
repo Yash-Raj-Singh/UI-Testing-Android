1712fed7a9994d0815ebcc38938d99170c953da6
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-11-25 14:16:29 +0530

    Learned testing Fragment in Isolation.....

---
+++ app/src/androidTest/java/com/example/uitesting/ui/movie/MovieDetailFragmentTest.kt
@@ -0,0 +1,35 @@
+package com.example.uitesting.ui.movie
+
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.example.uitesting.R
+import com.example.uitesting.data.DummyMovies.THE_RUNDOWN
+import com.example.uitesting.factory.MovieFragmentFactory
+import junit.framework.TestCase
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MovieDetailFragmentTest{
+    @Test
+    fun test_isMovieDataVisible() {
+        val movie = THE_RUNDOWN
+        val fragmentFactory = MovieFragmentFactory()
+        val bundle = Bundle()
+        bundle.putInt("movie_id", movie.id)
+
+        val scenario = launchFragmentInContainer<MovieDetailFragment>(
+            fragmentArgs = bundle,
+            factory = fragmentFactory
+        )
+
+        onView(withId(R.id.movie_title)).check(matches(withText(movie.title)))
+
+        onView(withId(R.id.movie_description)).check(matches(withText(movie.description)))
+    }
+}
---
+++ app/src/main/java/com/example/uitesting/data/source/MoviesDataSource.kt
@@ -0,0 +1,7 @@
+package com.example.uitesting.data.source
+
+import com.example.uitesting.data.Movie
+
+interface MoviesDataSource {
+    fun getMovie(movieId: Int): Movie?
+}
